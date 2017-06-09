9523fbc326831af748944eb81fd0a4b820098266
vishnuraman <vishnu310592@gmail.com>
2017-06-09 14:53:39 +0100

    algorithms

--- GUIBeliefNetwork/bfs.py
+++ GUIBeliefNetwork/bfs.py
@@ -1,4 +1,5 @@
 import Linking
+
 graph = {
     1: [2, 3, 4],
     2: [5, 6],
@@ -10,43 +11,54 @@ graph = {
 }
 
 
-def Bfs(graph_to_search, start, end):
+def Bfs(graph, start, end):
+    # keeps track of all paths to be checked
     queue = [[start]]
+    # keeps track of visited nodes
     visited = set()
 
+    # keeps looping until all possible paths are checked
     while queue:
         # Gets the first path in the queue
         path = queue.pop(0)
 
         # Gets the last node in the path
-        vertex = path[-1]
+        node = path[-1]
 
         # Checks if we got to the end
-        if vertex == end:
+        if node == end:
             return path
         # We check if the current node is already in the visited nodes set in order not to recheck it
-        elif vertex not in visited:
+        elif node not in visited:
+
             # enumerate all adjacent nodes, construct a new path and push it into the queue
-            for m in graph_to_search.vert_dict:
-                for n in graph_to_search.vert_dict[m].adjacent:
+
+            for m in graph.vert_dict:
+
+                for n in graph.vert_dict[m].adjacent:
                     new_path = list(path)
                     new_path.append(n)
+                    #print(list(new_path))
                     queue.append(new_path)
 
-            # Mark the vertex as visited
-                    visited.add(vertex)
+                    # Mark the node as visited
+                visited.add(node)
 
 
-
-LK=Linking.Graph()
+LK = Linking.Graph()
 LK.add_vertex(0)
 LK.add_vertex(1)
 LK.add_vertex(2)
 LK.add_vertex(3)
 LK.add_vertex(4)
-LK.add_edge(0,1,1)
-LK.add_edge(0,2,1)
-LK.add_edge(1,3,1)
-LK.add_edge(1,4,1)
+LK.add_edge(0, 1, 1)
+LK.add_edge(0, 2, 1)
+LK.add_edge(1, 3, 1)
+LK.add_edge(1, 4, 1)
 
-print(list(Bfs(LK, 0, 4)))
+for v in list(LK):
+    print (str(v.get_id()) + str(LK.vert_dict[v.get_id()]))
+
+# print(list(Bfs(LK, 0, 4)))
+
+print(Bfs(LK, 0, 4))
