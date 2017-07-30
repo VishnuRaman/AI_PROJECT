36626ac5f9abd1aebdede1e1dacf295dba54ad23
rasveerb <rasveerb@sky.com>
2017-07-30 23:12:45 +0100

    run method produces the correct structure for the min and max values

--- searching/minmaxgui.py
+++ searching/minmaxgui.py
@@ -9,7 +9,6 @@ import GuiArray,ManageNode,Algorithms
 from tkinter import *
 
 from tkinter.simpledialog import askstring
-from tkinter.messagebox import showinfo
 
 root = Tk()
 topFrame = Frame(root)
@@ -34,8 +33,8 @@ button1=Button(bottomFrame,text="Run")
 
 button2=Button(topFrame2,text="Enter value")
 
-button3=Button(topFrame2,text="<<")
-button4=Button(topFrame2,text=">>")
+button3=Button(topFrame2,text="Previous Step")
+button4=Button(topFrame2,text="Next Step")
 
 button1.pack(side=BOTTOM)
 button2.pack(side=LEFT)
@@ -64,44 +63,6 @@ box10=canvas.create_rectangle(795,490,845,440)
 box11=canvas.create_rectangle(875,490,925,440)
 box12=canvas.create_rectangle(955,490,1005,440)
 
-#lines leading up to min triangle
-#set 1
-line1=canvas.create_line(40,440,120,290)
-line2=canvas.create_line(120,440,120,290)
-line3=canvas.create_line(200,440,120,290)
-#min triangle
-min1=canvas.create_polygon(85,240,120,290,155,240, fill="dark green")
-minLine1=canvas.create_line(120,240,510,90)
-
-#set 2
-line4=canvas.create_line(300,440,380,290)
-line5=canvas.create_line(380,440,380,290)
-line6=canvas.create_line(460,440,380,290)
-#min triangle
-min2=canvas.create_polygon(345,240,380,290,415,240, fill="dark green")
-minLine2=canvas.create_line(380,240,510,90)
-
-
-#set 3
-line7=canvas.create_line(560,440,640,290)
-line8=canvas.create_line(640,440,640,290)
-line9=canvas.create_line(720,440,640,290)
-#min triangle
-min3=canvas.create_polygon(605,240,640,290,675,240, fill="dark green")
-minLine3=canvas.create_line(640,240,510,90)
-
-#set 4
-line10=canvas.create_line(820,440,900,290)
-line11=canvas.create_line(900,440,900,290)
-line12=canvas.create_line(980,440,900,290)
-#min triangle
-min4=canvas.create_polygon(865,240,900,290,935,240, fill="dark green")
-minLine4=canvas.create_line(900,240,510,90)
-
-#max triangle
-max=canvas.create_polygon(475,90,510,40,545,90, fill="dark red")
-
-
 def popupValue(e):
 
     # provides pop up box
@@ -127,16 +88,48 @@ def ValueEntry(event):
 def Run(event):
     root.config(cursor="")
 
-    #produce the lines which lead to the next part for min
+    # lines leading up to min triangle
+    # set 1
+    line1 = canvas.create_line(40, 440, 120, 290)
+    line2 = canvas.create_line(120, 440, 120, 290)
+    line3 = canvas.create_line(200, 440, 120, 290)
+    # min triangle
+    min1 = canvas.create_polygon(85, 240, 120, 290, 155, 240, fill="dark green")
+    minLine1 = canvas.create_line(120, 240, 510, 90)
+
+    # set 2
+    line4 = canvas.create_line(300, 440, 380, 290)
+    line5 = canvas.create_line(380, 440, 380, 290)
+    line6 = canvas.create_line(460, 440, 380, 290)
+    # min triangle
+    min2 = canvas.create_polygon(345, 240, 380, 290, 415, 240, fill="dark green")
+    minLine2 = canvas.create_line(380, 240, 510, 90)
+
+    # set 3
+    line7 = canvas.create_line(560, 440, 640, 290)
+    line8 = canvas.create_line(640, 440, 640, 290)
+    line9 = canvas.create_line(720, 440, 640, 290)
+    # min triangle
+    min3 = canvas.create_polygon(605, 240, 640, 290, 675, 240, fill="dark green")
+    minLine3 = canvas.create_line(640, 240, 510, 90)
+
+    # set 4
+    line10 = canvas.create_line(820, 440, 900, 290)
+    line11 = canvas.create_line(900, 440, 900, 290)
+    line12 = canvas.create_line(980, 440, 900, 290)
+    # min triangle
+    min4 = canvas.create_polygon(865, 240, 900, 290, 935, 240, fill="dark green")
+    minLine4 = canvas.create_line(900, 240, 510, 90)
+
+    # max triangle
+    max = canvas.create_polygon(475, 90, 510, 40, 545, 90, fill="dark red")
 
 
-    #produce the lines which lead to the top max
 
     #run the algorithms for min and max and enter the data into the correct spaces on the gui
 
 
 button1.bind("<Button-1>",Run)
-# box1.bind("<Button1>",ValueEntry)
 button2.bind("<Button-1>",ValueEntry)
 
 root.mainloop()
